0a779b129169b66a82a65a6882d7cdbcfe4f8172
sharaseaman <shara.seaman@gmail.com>
2017-09-08 16:28:43 -0500

    started compare module

--- modules/compare.js
+++ modules/compare.js
@@ -1,3 +1,24 @@
-function compare ( random, playerOne, playerTwo, playerThree, playerFour) {
-    if (random === )
+function compare ( random, playerArray) {
+    var results = [];
+    for (var i = 0; i < playerArray.length; i++) {
+        if (random === playerArray[i]) {
+            if (playerArray[i] === playerArray[0]){
+                var player1 = "You got it!";
+                results.push(player1);
+            } else if (playerArray[i] === playerArray[1]){
+                var player2 = "You got it!";
+                results.push(player2);
+            } else if (playerArray[i] === playerArray[2]){
+                var player3 = "You got it!";
+                results.push(player3);
+            } else {
+                var player2 = "You got it!";
+                results.push(player2);
+        } else 
+        
+    }
 }
+
+// this wont work
+// maybe input the post objects for player input into an array
+// add a new property to each object with the result
